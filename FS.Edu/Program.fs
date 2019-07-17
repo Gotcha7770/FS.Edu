36d7c8259803eb0e8430719b33e0002b7835b545
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2019-07-17 15:44:26 +0300

    simple examples

--- FS.Edu/Program.fs
+++ FS.Edu/Program.fs
@@ -2,6 +2,25 @@
 
 open System
 
+//свой оператор не равно
+let (!=) x y = x <> y
+
+//форма записи для таблицы истинности
+let andTruthTable x y =
+    match x, y with
+    | true, true-> true
+    | _, _ -> false
+
+let orTruthTable x y =
+    match x, y with
+    | false, false -> false
+    | _, _ -> true
+
+let xorTruthTable x y =
+    match x, y with
+    | x, y when x != y -> true
+    | _, _ -> false
+
 // Выводит таблицу истинности для заданной функции
 let printTruthTable f =
     printfn "      |  true | false |"
@@ -10,6 +29,39 @@ let printTruthTable f =
     printfn " false|  %5b  |  %5b  |" (f false true) (f false false)
     printfn "      +-------+-------+"
     printfn "\n"
+
+// Размеченное объединение, представляющее масть карты
+type Suit =
+    | Hearts
+    | Diamonds
+    | Spades
+    | Clubs
+
+type Rank = 
+    /// Represents the rank of cards 2 .. 10
+    | Value of int
+    | Ace
+    | King
+    | Queen
+    | Jack
+
+    static member GetAllRanks() = 
+        [ yield Ace
+          for i in 2 .. 10 do yield Value i
+          yield Jack
+          yield Queen
+          yield King ]
+
+type Card = { Suit: Suit; Rank: Rank }
+
+let fullDeck = 
+    [ for suit in [ Hearts; Diamonds; Clubs; Spades] do
+          for rank in Rank.GetAllRanks() do 
+              yield { Suit=suit; Rank=rank } ]
+
+type BinaryTree =
+    | Node of int * BinaryTree * BinaryTree
+    | Empty
 
 [<EntryPoint>]
 let main argv =
