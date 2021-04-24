4629e0456d82a0a28326ae74638a4ce3ce157eb0
Gotcha7770 <gbakanov@inbox.ru>
2021-04-24 16:04:49 +0300

    execute process module

---
+++ FS.Edu.Core/ExecuteProcess.fs
@@ -0,0 +1,40 @@
+﻿module FS.Edu.ExecuteProcess
+
+open System.Diagnostics
+open System.Text
+
+type ProcessResult =
+    { exitCode: int
+      stdout: string
+      stderr: string }
+
+let private executeFromInfo (procInfo: ProcessStartInfo) =
+    let proc = Process.Start(procInfo)
+
+    let output = StringBuilder()
+    proc.OutputDataReceived.Add(fun args -> output.Append(args.Data) |> ignore)
+    let error = StringBuilder()
+    proc.ErrorDataReceived.Add(fun args -> error.Append(args.Data) |> ignore)
+
+    proc.BeginErrorReadLine()
+    proc.BeginOutputReadLine()
+    proc.WaitForExit()
+
+    { exitCode = proc.ExitCode
+      stdout = output.ToString()
+      stderr = error.ToString() }
+
+let executeProcess name args =
+    let procInfo = ProcessStartInfo(name, args)
+    procInfo.RedirectStandardOutput <- true
+    procInfo.RedirectStandardError <- true
+    executeFromInfo procInfo
+
+let executeProcessSilently name args =
+    let procInfo = ProcessStartInfo(name, args)
+    procInfo.UseShellExecute <- false
+    procInfo.RedirectStandardOutput <- true
+    procInfo.RedirectStandardError <- true
+    procInfo.CreateNoWindow <- true
+
+    executeFromInfo procInfo
