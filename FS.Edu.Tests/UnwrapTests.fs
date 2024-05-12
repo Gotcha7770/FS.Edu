55bd57497146c05c09bb563166d631b24686525f
Gotcha7770 <gbakanov.box@gmail.com>
2024-05-12 18:09:11 +0300

    unwrap with ce

--- FS.Edu.Tests/UnwrapTests.fs
+++ FS.Edu.Tests/UnwrapTests.fs
@@ -4,14 +4,13 @@ open Swensen.Unquote
 open Xunit
 
 let computation: Async<Result<int option, string>> =
-        let option = Some 42
+        let option = Some 2
         let result = Ok option
         async.Return result
     
 [<Fact>]
 let ``Unwrap it in procedural style`` =
-    let async = computation
-    let result = async |> Async.RunSynchronously
+    let result = computation |> Async.RunSynchronously
     let option =
         match result with
         | Ok x -> x
@@ -20,14 +19,34 @@ let ``Unwrap it in procedural style`` =
         match option with
         | Some x -> x
         | None -> 0
+
     test <@ value = 42 @>
-    
+
 [<Fact>]
-let ``Unwrap with ce`` =
-    let ce =  async {
-        let! result = computation
-        return result |> Result.map      
+let ``Unwrap with ce`` =        
+    let unwrapResult asyncValue =  async {
+        match! asyncValue with
+        | Ok option -> return option
+        | _ -> return  Some -1
+    }
+
+    let unwrapOption asyncValue =  async {
+        match! asyncValue with
+        | Some value -> return value
+        | None -> return  0
+    }
+    
+    let ce = async {
+        let! option = async {
+            match! computation with
+            | Ok option -> return option
+            | _ -> return  Some -1
+        }
+        return match option with
+                | Some x -> x
+                | None -> 0
     }
     
     let value = ce |> Async.RunSynchronously
+
     test <@ value = 42 @>
