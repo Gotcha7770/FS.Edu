bce94bdd4f1117b6ceac88485a670616262ef55d
Gotcha7770 <gbakanov.box@gmail.com>
2024-09-16 13:10:12 +0300

    nullability in F#

---
+++ FS.Edu.Core/Nullability.fs
@@ -0,0 +1,15 @@
+﻿module FS.Edu.Nullability
+
+open System
+
+let tmp = null
+
+type Email = Email of string
+type Person = {
+    Name: string
+    Email: Email
+}
+
+let email = Email null
+//let person = { Name = null; Email = null;  }
+let date = DateTime.Parse null 
